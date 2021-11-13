24e1aace04f7212be1f0805bf0f46c993fdc90eb
Apoorva Bhatnagar <bhatnagar.apoorva12@gmail.com>
2021-11-12 18:30:14 -0800

    Updated code to include next buttons

--- src/components/GifSearch.tsx
+++ src/components/GifSearch.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import GifDetails from "./GifDetails";
 
 interface IGifResponse {
@@ -12,10 +14,12 @@ interface IGifResponse {
 function GifSearch() {
     const apiKey = "SyBnCaulCHSRBuCc4dAc2VE4lb1HESJf";
     const [gifs, setGifs] = useState<IGifResponse[]>([]);
+    const [isPreviousDisabled, setIsPreviousDisabled] = useState(true);
+    const [offsetCount, setOffsetCount] = useState(0);
     const [inputText, setInputText] = useState("");
     const [searchText, setSearchText] = useState("surprise");  
     
-    const requestUri = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchText}&limit=10`;
+    const requestUri = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchText}&limit=10&offset=${offsetCount}`;
     
     const fetchGifs = async () => {
         const response = await fetch(requestUri);
@@ -29,23 +33,62 @@ function GifSearch() {
         // eslint-disable-next-line
     }, [searchText]);
 
+    useEffect(() => {
+        fetchGifs()
+        // eslint-disable-next-line
+    }, [offsetCount]);
+
     const onSubmit = (e: any) => {
         e.preventDefault();
         setSearchText(inputText);
         console.log(inputText);
     }
 
-    return (<div className="app">
-    <form onSubmit={onSubmit} className="search-form">
-      <input className="search-bar" type="text" value={inputText} onChange={e => setInputText(e.target.value)} ></input>
-      <button className="search-button" type="submit">Search</button>      
-    </form>    
-    <div className="gifs">
-        {gifs.map(each => (              
-          <GifDetails key={each.id} title={each.title} imageUri={each.images.original.url}></GifDetails>
-        ))}            
-    </div>
-</div>)
+    const getPreviousData = () =>
+    {
+        if(offsetCount > 0) 
+        {
+            setOffsetCount(offsetCount-10);
+            fetchGifs();
+        }
+        
+        if(offsetCount === 0)
+        {
+            setIsPreviousDisabled(true);
+        }
+    };
+
+    const getNextData = () =>
+    {
+        setIsPreviousDisabled(false);
+        setOffsetCount(offsetCount+10);
+        fetchGifs();
+    };
+
+    return (
+    <div className="app">
+        <form onSubmit={onSubmit} className="search-form">
+        <input className="search-bar" type="text" value={inputText} onChange={e => setInputText(e.target.value)} ></input>
+        <button className="search-button" type="submit">Search</button>      
+        </form>    
+        <div className="gifs">
+            {gifs.map(each => (              
+            <GifDetails key={each.id} title={each.title} imageUri={each.images.original.url}></GifDetails>
+            ))}            
+        </div>
+        <div className="next-previous-buttons">
+            <div className="previous-button">
+                <button className="buttons" onClick={e => getPreviousData()} disabled={isPreviousDisabled}><FontAwesomeIcon icon={faArrowLeft} />
+                <p>PREV</p>
+                </button>            
+            </div>
+            <div className="next-button">
+                <button className="buttons" onClick={e => getNextData()}><FontAwesomeIcon icon={faArrowRight} />
+                <p>NEXT</p>
+                </button>
+            </div>
+        </div>
+    </div>)
 
 }
 

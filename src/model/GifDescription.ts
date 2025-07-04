bf3cde4b31933786db0de950c75127ae67ec5a4b
Patricija <cer.patricija@gmail.com>
2025-07-04 15:35:42 -0700

    updated structure

--- src/model/GifDescription.ts
+++
@@ -1,9 +0,0 @@
-interface GifDescription
-{
-    imageUri: string | null,
-    originalImageUri: string | null,
-    description: string | null,
-    detectedText: string | null
-}
-
-export default GifDescription;
